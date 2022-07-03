99d852a27ed583ee0ffbf3e209f61abb07873550
Youssef Samaan <ysamaan2001@gmail.com>
2022-07-03 15:05:48 -0400

    added 2 localhosts

--- tradingWebsite/tradingWebsite/settings.py
+++ tradingWebsite/tradingWebsite/settings.py
@@ -29,7 +29,10 @@ SECRET_KEY = config("SECRET_KEY")
 DEBUG = config("DEBUG", cast=bool)
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'trading-practice.herokuapp.com'
+    '127.0.0.1'
+]
 
 
 # Application definition
