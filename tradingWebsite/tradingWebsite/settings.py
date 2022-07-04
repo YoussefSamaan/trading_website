03828b34627a16df939d1e08beb75d5d1422e362
Youssef Samaan <ysamaan2001@gmail.com>
2022-07-03 20:00:08 -0400

    fixed path in staticfiles_dir

--- tradingWebsite/tradingWebsite/settings.py
+++ tradingWebsite/tradingWebsite/settings.py
@@ -144,7 +144,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "/stocks/static")
+    os.path.join(BASE_DIR, "\\stocks\\static")
 ]
 
 
