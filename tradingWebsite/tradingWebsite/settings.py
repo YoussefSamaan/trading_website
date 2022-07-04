663c047471b4d7502539e49ed8be2ff6cfc952c1
Youssef Samaan <ysamaan2001@gmail.com>
2022-07-03 20:12:09 -0400

    hopefully this fixes the 500 error code

--- tradingWebsite/tradingWebsite/settings.py
+++ tradingWebsite/tradingWebsite/settings.py
@@ -149,7 +149,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 STATIC_URL = 'static/'
